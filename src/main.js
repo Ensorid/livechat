827d38910a0ceb22f2e5684e1adfac8694ab1d49
Ensorid <182452094+Ensorid@users.noreply.github.com>
2025-04-24 14:12:43 +0200

    Fix

--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
 console.log("Hello World")
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.get('/', (req, res) => {
-  res.sendFile("public/index.html")
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
